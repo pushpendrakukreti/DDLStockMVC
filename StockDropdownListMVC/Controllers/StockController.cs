5b7b165ef3b4a8b86a61e1d49e98af8658e1159c
PUSHPENDRA KUKRETI <KUKRETIARYAN@GMAIL.COM>
2019-08-22 00:49:25 +0530

    Final Check on DropdownList with Id in URL updates the value according to the DDL Element

---
+++ StockDropdownListMVC/Controllers/StockController.cs
@@ -0,0 +1,45 @@
+﻿using StockDropdownListMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StockDropdownListMVC.Controllers
+{
+    public class StockController : Controller
+    {
+        // GET: Stock
+        public ActionResult AddOrEdit(int id=0)
+        {
+            Stock stockModel = new Stock();
+
+            using (dataModels dbModel = new dataModels())
+            {
+                if (id != 0)
+                    stockModel = dbModel.Stocks.Where(x => x.Id==id).FirstOrDefault();
+                stockModel.ProductCollection = dbModel.Products.ToList<Product>();
+            }
+
+            return View(stockModel);
+
+            //stockModel.ProductCollection = new List<Product>()
+            //{
+            //    new Product()
+            //    {
+            //        ProductId=1,ProductName="Computer"
+            //    },
+            //    new Product()
+            //    {
+            //        ProductId=2,ProductName="Mobile"
+            //    },
+            //};
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Stock stock)
+        {
+            return View();
+        }
+    }
+}
